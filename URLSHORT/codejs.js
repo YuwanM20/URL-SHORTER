afb4ce4ebf2bc718b3e01f5df639207de05ff43d
yuwann <95163223+YuwanM20@users.noreply.github.com>
2023-11-14 22:05:47 +0700

    Add files via upload

---
+++ URLSHORT/codejs.js
@@ -0,0 +1,31 @@
+let btn = document.getElementById("shorten");
+
+btn.addEventListener('click', short);
+
+
+async function short(){
+    let longURL = document.getElementById("longurl").value;
+    let shortURL = document.getElementById("shorturl");
+
+    const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longURL}`);
+    const data = await result.json();
+
+    shortURL.value = data.result.short_link2;
+}
+
+// copy url
+let newURL = document.getElementById("shorturl");
+let coptButton = document.getElementById("copy");
+
+coptButton.onclick = ()=>{
+    newURL.select();
+
+    window.navigator.clipboard.writeText(newURL.value);
+
+    Swal.fire({
+        title: "Url Telah Disalin",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 3000 // Menampilkan selama 3 detik
+    });
+}
